94b0498e2c1e31647a6f2e76c70f3c40668eacd8
Luis Garcia <luisgarcia26284@gmail.com>
2023-11-30 11:57:55 +0800

    Paritioning is working fine.
    Be careful with decimal->float conversion. We need to make sure this is not gettin gin the way of precision.

--- sampleddetection/samplers/window_sampler.py
+++ sampleddetection/samplers/window_sampler.py
@@ -6,14 +6,18 @@ from random import shuffle
 from typing import Tuple
 
 import numpy as np
-from scapy.all import PcapReader, rdpcap
-from scapy.plist import Packet, PacketList
+from scapy.all import Packet, PcapReader, rdpcap, wrpcap
+from scapy.plist import PacketList
 from tqdm import tqdm
 
 from ..utils import setup_logger
 
 # import PcaketList
-MAX_MEM = 4.0  # GB This is as mcuh as we want in ram at once
+# MAX_MEM = 4e9  # GB This is as mcuh as we want in ram at once
+# MAX_MEM = 250e6  # GB This is as mcuh as we want in ram at once
+# MAX_MEM = 100e6  # GB This is as mcuh as we want in ram at once
+# MAX_MEM = 2 ** (26.575)
+MAX_MEM = 2 ** (23.25)
 
 
 class CaptureReader:
@@ -51,10 +55,12 @@ class CaptureReader:
             check if passed file needs partition
             if not just it as is
         """
+        self.should_partition = self.lf_size > MAX_MEM
         num_files = len(list(self.dst_dir.glob("*.pcap")))
         if num_files > 0:
             # Find the json info object
             json_info = self.dst_dir / "info.json"
+            self.should_partition = False
             with open(json_info, "r") as f:
                 self.parttn_info = json.load(
                     f
@@ -64,7 +70,6 @@ class CaptureReader:
         else:
             self.dst_dir.mkdir(parents=True, exist_ok=True)
             # Make decision on whether to partition or not
-            self.should_partition = self.lf_size > MAX_MEM
             if not self.should_partition:
                 self.cur_cptfile_ptr = rdpcap(str(self.lf_location))
                 self.cur_cptfile_name = self.lf_location
@@ -79,17 +84,17 @@ class CaptureReader:
     @property
     def last_sniff_time(self):
         assert len(self.parttn_info) != 0, "Partitions not loaded"
-        return self.parttn_info["captures"][-1]["last_sniff_time"]
+        return list(self.parttn_info["captures"].values())[-1]["last_sniff_time"]
 
     # TODO: likewise here
     @property
     def first_sniff_time(self):
         assert len(self.parttn_info) != 0, "Partitions not loaded"
-        return self.parttn_info["captures"][0]["last_sniff_time"]
+        return list(self.parttn_info["captures"].values())[0]["last_sniff_time"]
 
     def __len__(self) -> int:
         assert len(self.cur_cptfile_ptr) == 0, "No currently loaded partition"
-        return self.parttn_info["captures"][-1]["last_idx"] + 1
+        return list(self.parttn_info["captures"].values())[-1]["last_idx"] + 1
 
     def partition(self) -> None:
         """
@@ -101,6 +106,7 @@ class CaptureReader:
             return None
         if not self.should_partition:
             self.logger.warn(f"Will not partition, file within {MAX_MEM} budget")
+            return
 
         pcap_rdr = PcapReader(str(self.lf_location))
         amnt_of_paritions = ceil(self.lf_size / MAX_MEM)
@@ -108,7 +114,7 @@ class CaptureReader:
         # IMPORTANT: Dictionary below will contain info you need to know how to
         # parse through partitions.
         self.parttn_info = {
-            "src_file_loc": self.lf_location,
+            "src_file_loc": str(self.lf_location),
             "src_file_size": self.lf_size,
             "amnt_of_paritions": amnt_of_paritions,
             "partition_size_max": MAX_MEM,
@@ -116,41 +122,52 @@ class CaptureReader:
         }
 
         # Read each packet inside self.capture_ptr until we reach 1g
-        partition_bar = tqdm(total=amnt_of_paritions, desc="Partitioning Files")
+        inter_ptt_bar = tqdm(
+            total=amnt_of_paritions, desc="Partitioning Files", leave=True, position=0
+        )
         ptt_first_idx = 0
         ptt_last_idx = 0
         for i in range(amnt_of_paritions):
-            new_file = open(self.dst_dir / f"capture_{i}.pcap", "wb")
             size_of_file = 0
-
+            intra_ptt_bar = tqdm(
+                total=MAX_MEM, desc=f"Partition {i}", leave=False, position=1
+            )
             packet = pcap_rdr.read_packet()
-            earliest_sniff = packet.time
+            # CHECK: Json is not letting me dump `Decimal` types which
+            #   packet.time is returning. Im afraid this might get in the way of precision.
+            #   To get this draft finished Ill let it go for now but be careful
+            #   Ill mark the two lines of relevant with "DTF"(decimal-to-float)
+            earliest_sniff = float(packet.time)  # CHECK: DTF
 
+            packets = []
             while packet != None:
-                new_file.write(packet)
-                size_of_file += len(
-                    packet
-                )  # CHECK: make sure this s accurately working
+                packets.append(packet)
+                size_of_file += len(packet)
                 ptt_last_idx += 1
+                intra_ptt_bar.update(len(packet))
                 if size_of_file > MAX_MEM:
                     break
                 else:  # Update earliest sniff time
-                    packet = pcap_rdr.read_packet()
+                    try:
+                        packet = pcap_rdr.read_packet()
+                    except EOFError:  # EOF
+                        break
+            # write packets to pcap
+            wrpcap(str(self.dst_dir / f"capture_{i}.pcap"), packets)
 
-            new_file.close()
             self.parttn_info["captures"][f"capture_{i}.pcap"] = {
                 "size_of_file": size_of_file,
                 "first_sniff_time": earliest_sniff,
-                "last_sniff_time": packet.time,
+                "last_sniff_time": float(packet.time),  # CHECK: DTF
                 "first_idx": ptt_first_idx,
                 "last_idx": ptt_last_idx - 1,
             }
 
             ptt_first_idx = ptt_last_idx
-            partition_bar.update(1)
+            inter_ptt_bar.update(1)
 
         # Save for later use
-        with open(self.dst_dir / "info.json", "w") as f:
+        with open(str(self.dst_dir / "info.json"), "w") as f:
             json.dump(self.parttn_info, f)
 
         self.logger.debug(
@@ -163,7 +180,7 @@ class CaptureReader:
         Make sure you have partitioned
         """
         assert (
-            len(self.cur_cptfile_ptr) != 0
+            len(self.parttn_info) != 0
         ), "Capture file is not loaded. Make sure you partition."
         pattn, name = self._get_pattn(idx)
         local_idx = idx - self.parttn_info[name]["name"]["first_idx"]
@@ -182,7 +199,6 @@ class CaptureReader:
                     )  # FIX: fix these ambivalent return
                 else:
                     # Remove currently loaded file and load new one
-                    self.cur_cptfile_ptr.close()  # type:ignore #CHECK: if we can ignore
                     self.cur_cptfile_ptr = rdpcap(str(self.dst_dir / ptt_name))
                     self.cur_cptfile_name = ptt_name
                     return self.cur_cptfile_ptr, self.cur_cptfile_name
@@ -191,23 +207,23 @@ class CaptureReader:
 
 
 class UniformWindowSampler:
-    PARTITION_LOC = "./partitions"
-
     def __init__(
         self,
         path: str,
         window_skip: float = 1.0,
         window_length: float = 1.0,
         amount_windows: int = 100,
+        partition_loc: str = "./partitions",
     ):
         """
-        Sampler that will uniformly sample windows from the capture file.
+        Sampler will uniformly sample windows from the capture file.
         Args:
             path (str): Path to the .pcap file
             window_skip (float, optional): Time to skip between windows. Defaults to 1.0.
             window_length (float, optional): Length of each window. Defaults to 1.0.
             amount_windows (int, optional): Amount of windows to sample. Defaults to 100.
             locating_delta(float, optional): Delta used to determine if we have come close to a packet or not.
+            partition_loc (str, optional): Location of the partitions. Defaults to "./partitions".
         """
         # Ensure path exists
         assert Path(path).exists(), "The provided path does not exists"
@@ -217,9 +233,14 @@ class UniformWindowSampler:
         self.window_skip = window_skip
         self.window_length = window_length
 
+        # Structure doing most of the heavy lifting
+        self.caprdr = CaptureReader(
+            Path(path), Path(partition_loc)  # type:ignore CHECK:
+        )
+        self.caprdr.partition()
+
         # Locate and Read the file
         self.logger.info("⚠️ Loading the capture file. This will likely take a while")
-        self.caprdr = CaptureReader(Path(path), Path(PARTITION_LOC))
         self.first_ts = self.caprdr.first_sniff_time
         self.last_ts = self.caprdr.last_sniff_time
         self.logger.info("✅ Loaded the capture file.")
