8eef90057578e0b0e546ed777eaf0383413b9272
Luis Garcia <luisgarcia26284@gmail.com>
2024-03-20 13:17:44 +0800

    Passed: first tests.

--- test/conftest.py
+++ test/conftest.py
@@ -2,12 +2,6 @@ def pytest_addoption(parser):
     parser.addoption(
         "--csvfile",
         action="store",
-        default="./bigdata/Wednesday.csv",
+        default="./bigdata/specific_instances/40.83.143.209_192.168.10.14_443_49461.csv",
         help="Location to Csvfile",
-    )
-    parser.addoption(
-        "--no_of_alphanum",
-        action="store",
-        default="0",
-        help="Number of alphanumeric characters",
     )
--- test/test_entireflow.py
+++ test/test_entireflow.py
@@ -15,6 +15,7 @@ sys.path.append(parent_path)
 from logging import DEBUG as LVL_DEBUG
 from logging import INFO as LVL_INFO
 
+from sampleddetection.datastructures.flowsession import SampledFlowSession
 from sampleddetection.utils import NpEncoder, setup_logger
 
 global logger
@@ -32,30 +33,26 @@ def test_flowbytes(csvreader: CSVReader):
     dws = DynamicWindowSampler(csvrdr=csvreader)
     first_sniff = dws.csvrdr.first_sniff_time - 1
     final_sniff = dws.csvrdr.last_sniff_time
-    logger.info(f"First and last sniffs are {first_sniff} -> {final_sniff}")
     non_existant_window = 1e-16
     flowsesh = dws.sample(
         first_sniff,
         non_existant_window,
         final_sniff - first_sniff + non_existant_window + 1,
     )
-    logger.info(f"Done with sampling. Getting data from sample ")
     data = flowsesh.get_data()
-    sfp = next(iter(flowsesh.flows.values())).packets  # single flowpackets
-    logger.info(f"sfp son is of type {type(sfp[0])}")
-    pretty_packets = json.dumps([s[0].row.to_dict() for s in sfp], indent=4)
-    pretty_str = json.dumps(
-        {str(t): v for t, v in data.items()}, indent=4, cls=NpEncoder
-    )
-    logger.debug(f"Raw data dawg: \n{pretty_str}")
-    logger.debug(f"Packets for this flow are:\n{pretty_packets}")
     first_key = next(iter(data))
-    logger.info(f"First key is {first_key}")
-    bytes_length = data[first_key]["flow_byts_s"]
-    print(f"Data is \n{data}")
+    bytes_length = (
+        data[first_key]["totlen_fwd_pkts"] + data[first_key]["totlen_bwd_pkts"]
+    )
     assert bytes_length == 142114  # DATA extracted from wireshark
 
 
 @pytest.fixture
 def csvreader(request) -> CSVReader:
     return CSVReader(Path(request.config.getoption("--csvfile")))
+
+
+# @pytest.fixture
+# def sample(request) -> SampledFlowSession:
+#     src_ip =
+#     return CSVReader(
